deb633bb0013da271813a30b537d3dc91e407968
JS230310 <JS230310@falmouth.ac.uk>
2019-11-29 08:30:57 +0000

    Re-Adding my Tinkering Audio Project

---
+++ Joseph Spaul - Tinkering Audio/Assets/Scripts/CaptureFish.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptureFish : MonoBehaviour
+{
+    public AudioClip cash;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "BlackFish")
+        {
+            AudioSource.PlayClipAtPoint(cash, transform.position);
+            ScoreScript.scoreValue += 10;
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.tag == "RedFish")
+        {
+            AudioSource.PlayClipAtPoint(cash, transform.position);
+            ScoreScript.scoreValue += 5;
+            Destroy(col.gameObject);
+        }
+
+        if (col.gameObject.tag == "YellowFish")
+        {
+            AudioSource.PlayClipAtPoint(cash, transform.position);
+            ScoreScript.scoreValue += 1;
+            Destroy(col.gameObject);
+        }
+    }
+
+}
+
---
+++ Joseph Spaul - Tinkering Audio/Assets/Scripts/SlowFish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowFish : MonoBehaviour
+{
+    // Added Audio to fish getting hit
+    public AudioClip Hurt;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Projectile")
+        {
+            AudioSource.PlayClipAtPoint(Hurt, transform.position);
+            gameObject.GetComponent<FishMoving>().speed = 0;
+        }
+    }
+
+}
+
